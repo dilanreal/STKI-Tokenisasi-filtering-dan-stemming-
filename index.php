db46fb41040205873a67add0b606a271e61e496c
dilanreal <kuliahkuunisbank@gmail.com>
2020-10-19 17:30:23 +0700

    test

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+include_once 'lib/Enhanced_CS.php';
+include('include/navi.php');
+?>
+
+<!--jumbotron-->
+<div class="jumbotron text-center">
+  <h1>Sistem Temu Kembali Informasi</h1>
+  <h2>Tokenisasi, Stopword Removal dan Stemming</h2>
+</div>
+
+<div class="container pt">
+        <div class="row mt">
+            <div class="col-lg-6 col-lg-offset-3 centered">
+                <h3> Apa itu Sistem Temu Kembali Informasi? </h3>
+            </div>
+  <br>
+  <br>
+  <hr>
+                <p align="justify">Sistem temu balik informasi (information retrieval) digunakan untuk menemukan kembali informasi-informasi yang relevan terhadap kebutuhan pengguna dari suatu kumpulan informasi secara otomatis. Salah satu aplikasi umum dari sistem temu kembali informasi adalah search-engine atau mesin pencarian yang terdapat pada jaringan internet. Pengguna dapat mencari halaman-halaman Web yang dibutuhkannya melalui mesin tersebut.
+
+                Ukuran efektivitas pencarian ditentukan oleh precision dan recall. Precision adalah rasio jumlah dokumen relevan yang ditemukan dengan total jumlah dokumen yang ditemukan oleh search-engine. Precision mengindikasikan kualitas himpunan jawaban, tetapi tidak memandang total jumlah dokumen yang relevan dalam kumpulan dokumen.</p>
+
+               
+             </div>
+           </div>
+
+
---
+++ info.php
@@ -0,0 +1,17 @@
+<?php include('include/navi.php'); ?>
+
+<!--jumbotron-->
+<div class="jumbotron text-center">
+  <h1>Sistem Temu Kembali Informasi</h1>
+</div>
+
+<div class="container pt">
+        <div class="row mt">
+            <div class="col-lg-6 col-lg-offset-3 centered">
+                <center><p>Dilan Allya Barqi || 17.01.53.0070 </p>
+                        <hr>
+                <h4>Universitas Stikubank(Unisbank) Semarang</h4>
+                </center>
+               </div>
+             </div>
+           </div>
